b58cb367d8a9ed732af84e3459c4692b3a221ff9
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-07-07 18:32:08 -0500

    Crear navbar con array

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,13 +1,43 @@
 import { ActiveLink } from './ActiveLink'
 import styles from './Navbar.module.css'
 
+const menuItems = [
+    {
+        text: 'Home',
+        href: '/'
+    },
+    {
+        text: 'About',
+        href: '/about'
+    },
+    {
+        text: 'Contact',
+        href: '/contact'
+    },
+    {
+        text: 'Pricing',
+        href: '/pricing'
+    },
+];
 
 export const NavBar = () => {
     return (
-        <nav className={styles['menu-container']}>
-            <ActiveLink text="Home" href="/">Home</ActiveLink>
+        <nav className={styles['menu-container']} >
+            {
+                menuItems.map(({text, href}) => (
+                    <ActiveLink
+                    key={text}
+                    href={href}
+                    text={text}
+                    />
+
+                ))
+            }
+
+            {/* <ActiveLink key={menuItems.text} href={menuItems.href}>{menuItems.text}</ActiveLink>
             <ActiveLink text="About" href="/about">About</ActiveLink>
             <ActiveLink text="Contact" href="/contact">Contact</ActiveLink>
+            <ActiveLink text="Pricin" href="/pricing/pricing">Pricing</ActiveLink> */}
         </nav>
     )
 }
