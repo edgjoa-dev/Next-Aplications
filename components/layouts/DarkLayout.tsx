38ece2c2a9e8b72e4fba86bf49c3354d54f29509
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-07-07 19:46:15 -0500

    Cambio a TypeScript

--- components/layouts/DarkLayout.tsx
+++ components/layouts/DarkLayout.tsx
@@ -1,6 +1,7 @@
+import { FC } from 'react';
 import style from '../../styles/components/darkLayout.module.scss';
 
-export const DarkLayout = ({children}) => {
+export const DarkLayout: FC = ({children}) => {
     return(
         <div className={style['container']}>
         <h3>Dark Layout</h3>
--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,9 +1,10 @@
+import { FC } from "react";
 import Head from "next/head";
 import { NavBar } from "../NavBar";
 import styles from './layout.module.css'
 
 
-export const MainLayout = ({children}) => {
+export const MainLayout: FC = ({ children }) => {
     return(
         <div className={styles.container}>
             <Head>
